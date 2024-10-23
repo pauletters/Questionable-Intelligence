dd25d2a74f02a6c096bb7faf7521de261e2972a4
ventell virgo <lennyasme@gmail.com>
2024-10-22 21:17:47 -0400

    updated main.tsx, results page

---
+++ client/src/pages/Results.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Results: React.FC = () => {
+  return (
+    <div>
+      <h1>Form</h1>
+      <p>No results to show!</p>
+    </div>
+  );
+};
+
+
+export default Results;
